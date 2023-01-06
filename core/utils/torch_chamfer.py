640039045cd722d106e4c3dd32de45e4e301c3b7
Xingyu Lin <xingyu@berkeley.edu>
2023-01-05 18:00:04 -0800

    fixes

--- core/utils/torch_chamfer.py
+++ core/utils/torch_chamfer.py
@@ -1,7 +1,8 @@
 import torch
+
 def compute_chamfer_distance(x, y):
     """
-    Compute the chamfer distance between two point sets.
+    Compute the chamfer distance loss between two point sets.
     :param x: (B, N, 3) torch tensor
     :param y: (B, M, 3) torch tensor
     :return: (B,) torch tensor
@@ -9,4 +10,11 @@ def compute_chamfer_distance(x, y):
     x = x.unsqueeze(2).repeat(1, 1, y.shape[1], 1)
     y = y.unsqueeze(1).repeat(1, x.shape[1], 1, 1)
     dist = torch.sum((x - y) ** 2, dim=-1)
-    return torch.mean(torch.min(dist, dim=1)[0] + torch.min(dist, dim=2)[0])
+    dist1, _ = torch.min(dist, dim=1)
+    dist2, _ = torch.min(dist, dim=2)
+    return dist1.mean(dim=1) + dist2.mean(dim=1)
+
+if __name__ == '__main__':
+    x = torch.rand(512, 500, 3)
+    y = torch.rand(512, 200, 3)
+    print(compute_chamfer_distance(x, y).shape)
